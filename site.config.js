fdd0bc0c5f87b0ab7dad94da3629c1681829ff35
라 현 지 <rachaz@naver.com>
2025-01-27 17:49:01 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "raxchaz",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "IT Service Planner",
-    bio: "-",
+    bio: "https://github.com/raxchaz",
     email: "rachaz@naver.com",
     linkedin: "https://velog.io/@raxchaz",
     github: "https://github.com/raxchaz",
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://raxchaz.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
