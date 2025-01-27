093aaf00ff6210bf9d7120cb445db7730942c7fc
rachaz <rachaz@naver.com>
2025-01-27 18:01:33 +0900

    edit links

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "rachaz log",
+    description: "welcome to my channel!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
